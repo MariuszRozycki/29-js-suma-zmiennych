c53bb24367d8ab7b20c31cb80f015e2a6fd9ed74
mariansc83 <mariansc83@gmail.com>
2020-09-28 14:01:50 +0200

    Add exercise 29-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = 10;
+secondNumber = 12;
+
+function addParams(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+console.log(`Wynik dodawania parametrow funkcji addParams(firstNumber, secondNumber), to: ${addParams(firstNumber, secondNumber)}`);
